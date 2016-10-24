3399b5bdcae28ad67bb61ae16cdea779e7175b90
Tolokoban <fabien.petitjean@free.fr>
2016-10-24 19:43:00 +0200

    ...

---
+++ js/admin.js
@@ -0,0 +1,23 @@
+/** @module admin */require( 'admin', function(exports, module) { var _intl_={"en":{},"fr":{}},_$=require("$").intl;function _(){return _$(_intl_, arguments);}
+    "use strict";
+
+/**
+ * @module admin
+ *
+ * @description
+ * 
+ *
+ * @example
+ * var mod = require('admin');
+ */
+
+
+  
+module.exports._ = _;
+/**
+ * @module admin
+ * @see module:$
+ * @see module:admin
+
+ */
+});
