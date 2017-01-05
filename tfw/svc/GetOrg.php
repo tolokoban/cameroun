c987be83ee110abba7d7c91973d8315ebb30b98a
Tolokoban <fabien.petitjean@free.fr>
2017-01-05 21:46:41 +0100

    release

---
+++ tfw/svc/GetOrg.php
@@ -0,0 +1,12 @@
+<?php
+$ROLE = "";
+
+function execService() {
+    return Array(
+        "vaccins" => @file_get_contents("./pri/vaccins.org"),
+        "patient" => @file_get_contents("./pri/patient.org"),
+        "forms" => @file_get_contents("./pri/forms.org"),
+        "types" => @file_get_contents("./pri/types.org")
+    );
+}
+?>
