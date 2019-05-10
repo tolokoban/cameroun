8bdc14f190239e63b0df1615192fe72ee5ad9e5c
Fabien Petitjean <f.petitjean@lombardodier.com>
2019-05-10 18:06:47 +0200

    Date de consultation

--- src/mod/date.js
+++ src/mod/date.js
@@ -1,47 +1,50 @@
 "use strict";
 
+const SEC_TO_MS = 1000;
+const MS_TO_SEC = 1 / SEC_TO_MS;
+const BASE = 10;
 
 exports.now = function() {
-    return Math.floor( Date.now() * .001 );    
+    return Math.floor( Date.now() * MS_TO_SEC );
 };
 
 
 /**
  * @param {number} dat - Number of seconds since UNIX epoc (UTC).
- * @return Age in seconds.
+ * @return {number} Age in seconds.
  */
 exports.age = function( dat ) {
     return exports.now() - dat;
 };
 
 
-exports.toDate = function( dat ) {
-    return new Date( dat * 1000 );
+exports.toDate = function( datInSeconds ) {
+    return new Date( datInSeconds * SEC_TO_MS );
 };
 
 
 exports.toYMD = function( dat ) {
     if( typeof dat !== 'number' ) return null;
 
-    var d = exports.toDate( dat );
-    var Y = d.getFullYear();
-    var M = d.getMonth() + 1;
-    if( Y < 10 ) Y = '0' + Y;
-    var D = d.getDate() + 1;
-    if( D < 10 ) D = '0' + D;
-    
-    return Y + "-" + M + "-" + D;
+    const d = exports.toDate( dat );
+    const Y = d.getFullYear();
+    let M = d.getMonth() + 1;
+    if( M < BASE ) M = `0${M}`;
+    let D = d.getDate() + 1;
+    if( D < BASE ) D = `0${D}`;
+
+    return `${Y}-${M}-${D}`;
 };
 
 
 exports.fromYMD = function( ymd ) {
     if( typeof ymd !== 'string' ) return null;
-    if( ymd.length < 10 ) return null;
+    if( ymd.length < "YYYY-MM-DD".length ) return null;
 
-    var Y = parseInt( ymd.substr( 0, 4 ) );
-    var M = parseInt( ymd.substr( 5, 2 ) );
-    var D = parseInt( ymd.substr( 8, 2 ) );
-    var objDate = new Date( Y, M - 1, D );
+    const Y = parseInt( ymd.substr( 0, "YYYY".length ), BASE );
+    const M = parseInt( ymd.substr( "YYYY-".length, "MM".length ), BASE );
+    const D = parseInt( ymd.substr( "YYYY-MM-".length, "DD".length ), BASE );
+    const objDate = new Date( Y, M - 1, D );
 
-    return objDate.getTime() * .001;
+    return objDate.getTime() * MS_TO_SEC;
 };
